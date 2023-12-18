8e3dfb4051d4689e73ab64b0bbbe10a87f53c879
Giviko <chelidze.givia@gmail.com>
2023-12-19 01:02:46 +0400

    axios-client + static home page component setup

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import axiosClient from "./axios-client";
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [token, setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+
+  useEffect(() => {
+    if (!token) {
+      axiosClient.get("/token").then((response) => {
+        setToken(response.data.token);
+        localStorage.setItem("ACCESS_TOKEN", response.data.token);
+      });
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Banner />
+      <ContentList />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return (
+    <header className="header">
+      <img src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="Logo" />
+      <button className="login-btn">შესვლა</button>
+    </header>
+  );
+}
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h1>ბლოგი</h1>
+      <img src={process.env.PUBLIC_URL + "/images/banner.svg"} alt="Banner" />
+    </div>
+  );
+}
+
+function ContentList() {
+  return (
+    <>
+      <CategoryList />
+      <BlogsList />
+    </>
+  );
+}
+
+function CategoryList() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axiosClient
+      .get("/categories")
+      .then((response) => setCategories(response?.data?.data));
+  }, []);
+
+  return (
+    <div className="category-list-wrapper">
+      <ul className="category-list">
+        {categories &&
+          categories.map((category) => (
+            <Category
+              key={category.id}
+              color={category.text_color}
+              bgColor={category.background_color}
+            >
+              {category.title}
+            </Category>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+function Category({ children, color, bgColor }) {
+  return (
+    <button
+      className="category-btn"
+      style={{ color: color, backgroundColor: bgColor }}
+    >
+      {children}
+    </button>
+  );
+}
+
+function BlogsList() {
+  return <ul className="blog-list"></ul>;
+}
+
+function Blog({ blog }) {
+  return (
+    <li>
+      <img src="" alt="" />
+      <p>ავტორი</p>
+      <p>გამოქვეყნების თარიღი</p>
+      <p>ბლოგის სათაური</p>
+      <div>კატეგორიები</div>
+      <p></p>
+      <button>სრულად ნახვა</button>
+    </li>
+  );
+}
