a2dc3075182f8550560ff8ac64a14d09bb8e2aff
Giviko <chelidze.givia@gmail.com>
2023-12-29 02:32:24 +0400

    fixed helper.jsx which crashed netlify

--- blog/src/helper.jsx
+++ blog/src/helper.jsx
@@ -1,6 +1,9 @@
 import axiosClient from "./axios-client";
 
 export function filterBlogsByDate(blogs) {
+  // had an issue with netlify so i added 2 ifs
+  if (!blogs) return;
+  if (blogs.length === 0) return;
   return blogs.filter((blog) => {
     const currentDate = new Date();
     const publishDate = new Date(blog.publish_date);
